174c6446ee47bd03ad016b1d97f981950be97b65
Jonathan Chemla <jos.chemla@gmail.com>
2026-02-18 12:11:17 +0100

    allow importing more filetypes

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { devtools } from '@tanstack/devtools-vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     devtools(),
     react(),
-    tailwindcss()
+    tailwindcss(), 
+    nodePolyfills({ include: ['buffer'] }),
   ],
+  optimizeDeps: {
+    exclude: ['@loaders.gl/geopackage', '@loaders.gl/core', 'sql.js'],
+  },
+  ssr: {
+    noExternal: ['@loaders.gl/geopackage', '@loaders.gl/core'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
